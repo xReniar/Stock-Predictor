ff9e5498e5630f628b24d65627d94b84909cf7ab
xReniar <ilaorainer@gmail.com>
2024-06-03 16:32:35 +0200

    added file that creates model architecture

--- src/models.py
+++ src/models.py
@@ -1,6 +1,24 @@
 import torch
 from torch import nn
 
-class RNN(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super(RNN).__init__(*args, **kwargs)
+device = ("cuda" if torch.cuda.is_available() else "cpu")
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_stacked_layers) -> None:
+        super(LSTM).__init__()
+        self.hidden_size = hidden_size
+        self.num_stacked_layers = num_stacked_layers
+
+        self.lstm = nn.LSTM(input_size,
+                            hidden_size, 
+                            num_stacked_layers, 
+                            batch_first = True)
+        self.fc = nn.Linear(hidden_size,1)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        h0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        out, _ = self.lstm(x,(h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
