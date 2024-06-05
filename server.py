387e965975d2ab1a7c286dc1cca44786140e981e
xReniar <ilaorainer@gmail.com>
2024-06-05 21:49:18 +0200

    added "/stock"

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def default():
-    return render_template("default.html")
+    return render_template("stocks.html")
+
+@app.route("/stock/<stock>")
+def single_stock(stock):
+    return render_template("stock.html")
