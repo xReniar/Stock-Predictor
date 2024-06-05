4c1c0966b1f0673aee227448074b9e9bb721c295
xReniar <ilaorainer@gmail.com>
2024-06-05 21:48:16 +0200

    deleted

--- src/train.py
+++
@@ -1,9 +0,0 @@
-import models
-
-def train(model: models.LinearRegression | models.LSTM, X_train, y_train) -> models.LinearRegression | models.LSTM:
-    if type(model) == models.LSTM:
-        print("LSTM")
-
-    if type(model) == models.LinearRegression:
-        model.fit(X_train,y_train)
-        return model
