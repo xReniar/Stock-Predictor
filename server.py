997b2ab7ca79192e23753be46cec8c8941fb5c38
xReniar <ilaorainer@gmail.com>
2024-07-01 00:21:43 +0200

    removed unused requests

--- server.py
+++ server.py
@@ -8,29 +8,15 @@ app = Flask(__name__)
 def default():
     db = json.load(open("db.json"))
     stocks = [element["name"] for element in db["stocks"]]
+    stocks.sort()
     return render_template("stocks.html",
                            stocks=stocks)
 
-@app.route("/value/<stock>")
+@app.route("/chart/<string:stock>")
+def chart_page(stock):
+    return render_template("chart.html",
+                           stock=stock)
+
+@app.route("/value/<string:stock>")
 def value(stock):
     return str(get_current_price(stock))
-
-@app.route("/stock/<stock>")
-def single_stock(stock):
-    return render_template("stock.html")
-
-@app.route("/train/<model>/<stock>")
-def train(model,stock):
-    if model == "scikit-learn":
-        pass
-    if model == "pytorch":
-        pass
-
-@app.route("/predict/<model>/<stock>", methods=["POST"])
-def predict(model,stock):
-    if model == "scikit-learn":
-        pass
-    if model == "pytorch":
-        pass
-    data = request.to_json(force = True)
-    return str(data)
