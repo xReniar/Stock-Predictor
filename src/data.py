74e95b6b267e4efd7f16576b3eb90ff4d7a4b746
xReniar <ilaorainer@gmail.com>
2024-07-01 01:16:22 +0200

    added price-state function

--- src/data.py
+++ src/data.py
@@ -1,8 +1,15 @@
 import numpy as np
 import yfinance as yf
-
+from datetime import datetime,timedelta
+import time
 
 def get_current_price(symbol:str):
-    ticker = yf.Ticker(symbol)
-    todays_data = ticker.history(period='1d')
+    todays_data = yf.Ticker(symbol).history(period='1d')
     return todays_data['Close'].iloc[0]
+
+def get_stock_price_state(symbol:str):
+    data = yf.Ticker(symbol).history("5d")
+    return data["Close"].iloc[-1] - data["Close"].iloc[-2]
+
+def get_chart(symbol:str):
+    pass
