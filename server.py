ba3dc390ba5482ee04902c53ae2252e931563576
xReniar <ilaorainer@gmail.com>
2024-07-01 01:15:47 +0200

    added get method for stocks

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request
-from src.data import get_current_price
+from flask import Flask, render_template,jsonify
+from src.data import *
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def default():
+    return render_template("stocks.html")
+
+@app.route("/get_stocks")
+def get_stocks():
     db = json.load(open("db.json"))
-    stocks = [element["name"] for element in db["stocks"]]
-    stocks.sort()
-    return render_template("stocks.html",
-                           stocks=stocks)
+    stocks = sorted([element["name"] for element in db["stocks"]])
+    return jsonify(stocks)
 
 @app.route("/chart/<string:stock>")
 def chart_page(stock):
@@ -18,5 +20,9 @@ def chart_page(stock):
                            stock=stock)
 
 @app.route("/value/<string:stock>")
-def value(stock):
+def current_value(stock):
     return str(get_current_price(stock))
+
+@app.route("/price_state/<string:stock>")
+def price_state(stock):
+    return str(get_stock_price_state(stock))
