2b6df11990099016d029680a733eb2eaabab7f5d
xReniar <ilaorainer@gmail.com>
2024-07-01 23:27:17 +0200

    reading stocks first and then adding stocks

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,21 @@ import linear_regression,lstm
 import json
 
 db = json.load(open("../db.json"))
-stocks = db.keys()
+stocks = db["stocks"]
+for stock in stocks:
+    db[stock] = dict(
+        pytorch = dict(
+            value = 0,
+            mse = 0
+        ),
+        sklearn = dict(
+            value = 0,
+            mse = 0
+        )
+    )
+
+with open("../db.json","w") as f:
+    json.dump(db,f,indent=4)
 
 for stock in stocks:
     print(f"creating models for {stock}")
