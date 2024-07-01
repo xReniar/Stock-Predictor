6dd5c3a4e3ac325b1623a38e76b1b5384629c15f
xReniar <ilaorainer@gmail.com>
2024-07-01 17:59:23 +0200

    added function that returns the values of selected stock

--- src/data.py
+++ src/data.py
@@ -1,7 +1,7 @@
 import numpy as np
 import yfinance as yf
 from datetime import datetime,timedelta
-import time
+
 
 def get_current_price(symbol:str):
     todays_data = yf.Ticker(symbol).history(period='1d')
@@ -11,5 +11,13 @@ def get_stock_price_state(symbol:str):
     data = yf.Ticker(symbol).history("5d")
     return data["Close"].iloc[-1] - data["Close"].iloc[-2]
 
-def get_chart(symbol:str):
-    pass
+def get_chart_values(symbol:str):
+    data = yf.download(symbol,
+                   start=datetime.now() - timedelta(days=60),
+                   end=datetime.now(),
+                   interval="1d")
+    data = data.reset_index()
+    return dict(
+        xValues = [str(x.date()) for x in data["Date"].tolist()],
+        yValues = data["Close"].tolist()
+    )
