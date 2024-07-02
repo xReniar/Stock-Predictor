5bb80c682581ef5d709f63d952411bd9337e0f3b
xReniar <ilaorainer@gmail.com>
2024-07-02 18:30:57 +0200

    fixed path problem and added rnn training process

--- src/setup.py
+++ src/setup.py
@@ -1,20 +1,22 @@
 from data import get_models
-import linear_regression,lstm
+import linear_regression,lstm,rnn
 import json
 
-db = json.load(open("../db.json"))
+db = json.load(open("db.json"))
 stocks = db["stocks"]
+print(get_models())
 for model in get_models():
     model_dict = {}
     for stock in stocks:
         model_dict[stock] = dict(
-            predictions = 0,
+            predicted = 0,
             mse = 0
         )
-    with open(f"../result/{model}.json","w") as f:
+    with open(f"result/{model}.json","w") as f:
         json.dump(model_dict,f,indent=4)
 
 for stock in stocks:
     print(f"creating models for {stock}")
     linear_regression.main(stock)
     lstm.main(stock)
+    rnn.main(stock)
